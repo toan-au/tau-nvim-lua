d1d10ab40c0fc9af8a1dbb098e40d6ff50a5194e
Toan Au <toanau.dev@gmail.com>
2024-04-19 17:09:16 +1000

    setup autocompletion menu with LSP options

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,8 +3,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = {"lua", "javascript"},
-      sync_install = false,
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>a", function() print "hi" end)
 
